14c5ab9dfda3b3785f4719b5ec8e1f405efe7856
aka <akadev8@outlook.com>
2024-10-11 23:30:38 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+# src/main.py
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
 from datawave.httpget import DataWave
 
 if __name__ == "__main__":
